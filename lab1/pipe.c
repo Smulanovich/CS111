81a5ab6e11d049653b2ad7133b5ce70cf0a6bcd4
Santiago Mulanovich <santiagomulanovichsuarez@gmail.com>
2024-01-26 15:03:30 -0800

    update

--- lab1/pipe.c
+++ lab1/pipe.c
@@ -5,85 +5,98 @@
 #include <sys/wait.h>
 #include <errno.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
+    // Check if there are command line arguments
     if (argc < 2) {
-        fprintf(stderr, "Error: No programs specified.\n");
-        exit(EINVAL);
+        perror("Error: No programs specified");
+        exit(EINVAL);  // Exit with errno EINVAL for no programs specified
     }
 
-    int pipefd[2];
+    // Loop through each program in the command line arguments
+    for (int i = 1; i < argc; i++) {
+        // Fork a new process
+        pid_t pid = fork();
 
-    for (int i = 1; i < argc; ++i) {
-        if (i < argc - 1) {
-            // Create a pipe
-            if (pipe(pipefd) == -1) {
-                perror("Error in pipe");
+        // Check for fork error
+        if (pid == -1) {
+            perror("Fork failed");
+            exit(errno);  // Exit with the proper errno for fork failure
+        }
+
+if (pid == 0) {
+    // Child process
+
+    // If it's the first program, simply execute it
+    if (i == 1) {
+        if (execlp(argv[i], argv[i], (char *)NULL) == -1) {
+            perror("Exec failed");
+            exit(errno);
+        }
+    } else {
+        int pipefd[2];
+
+        // Create a pipe
+        if (pipe(pipefd) == -1) {
+            perror("Pipe creation failed");
+            exit(errno);
+        }
+
+        // Fork a new process for the current program
+        pid_t curr_pid = fork();
+
+        if (curr_pid == -1) {
+            perror("Fork failed");
+            exit(errno);
+        }
+
+        if (curr_pid == 0) {
+            // Child process of the current program
+
+            // Close the write end of the pipe
+            close(pipefd[1]);
+
+            // Redirect standard input to the pipe
+            dup2(pipefd[0], STDIN_FILENO);
+
+            // Execute the current program
+            if (execlp(argv[i], argv[i], (char *)NULL) == -1) {
+                perror("Exec failed");
+                exit(errno);
+            }
+        } else {
+            // Parent process (original child process of the first program)
+
+            // Close the read end of the pipe
+            close(pipefd[0]);
+
+            // Redirect standard output to the pipe
+            dup2(pipefd[1], STDOUT_FILENO);
+
+            // Execute the first program
+            if (execlp(argv[1], argv[1], (char *)NULL) == -1) {
+                perror("Exec failed");
                 exit(errno);
             }
         }
+    }
 
-        pid_t pid = fork();
+    // This part is reached only if there was an error in the exec calls
+    perror("Exec failed");
+    exit(errno);
+}
 
-        if (pid < 0) {
-            perror("Error in fork");
-            exit(errno);
-        } else if (pid == 0) {  // Child process
-            if (i > 1) {
-                // Check if the read end of the pipe is a valid file descriptor
-                if (dup2(pipefd[0], STDIN_FILENO) == -1) {
-                    perror("Error in dup2");
-                    exit(errno);
-                }
+    }
 
-                // Close the read end of the pipe
-                close(pipefd[0]);
-            }
+    // Parent process: Wait for all child processes to complete
+    for (int i = 1; i < argc; i++) {
+        int status;
+        pid_t child_pid = waitpid(-1, &status, 0);
 
-            if (i < argc - 1) {
-                // Check if the write end of the pipe is a valid file descriptor
-                if (dup2(pipefd[1], STDOUT_FILENO) == -1) {
-                    perror("Error in dup2");
-                    exit(errno);
-                }
-
-                // Close the write end of the pipe
-                close(pipefd[1]);
-            }
-
-            // Redirect standard error to the parent's standard error
-            if (dup2(STDERR_FILENO, STDERR_FILENO) == -1) {
-                perror("Error in dup2");
-                exit(errno);
-            }
-
-            // Execute program using execlp
-            execlp(argv[i], argv[i], (char *)NULL);
-
-            // If execlp fails
-            perror("Error in execlp");
-            exit(errno);
-        } else {
-            // Parent process
-            // Close both ends of the pipe in the parent
-            if (i < argc - 1) {
-                close(pipefd[0]);
-                close(pipefd[1]);
-            }
-
-            // Wait for the child process to terminate
-            int status;
-            waitpid(pid, &status, 0);
-
-            // Check if child process exited successfully
-            if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {
-                fprintf(stderr, "Error: Process %s failed with exit status %d\n", argv[i], WEXITSTATUS(status));
-                exit(WEXITSTATUS(status));
-            }
-
-            // Exit after the last command is executed
-            if (i == argc - 1) {
-                exit(0);
-            }
+        // Check for waitpid error
+        if (child_pid == -1) {
+            perror("Waitpid failed");
+            exit(errno);  // Exit with the proper errno for waitpid failure
         }
     }
 
