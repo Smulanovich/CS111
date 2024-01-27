a0ac9634943b89075061b28fbbdc61569bfcbcb9
Santiago Mulanovich <santiagomulanovichsuarez@gmail.com>
2024-01-26 16:39:05 -0800

    update

--- lab1/pipe.c
+++ lab1/pipe.c
@@ -7,96 +7,76 @@
 
 int main(int argc, char *argv[])
 {
-    // Check if there are command line arguments
     if (argc < 2) {
-        perror("Error: No programs specified");
-        exit(EINVAL);  // Exit with errno EINVAL for no programs specified
+        perror("Not enough arguments");
+        exit(EINVAL);
     }
 
-    // Loop through each program in the command line arguments
-    for (int i = 1; i < argc; i++) {
-        // Fork a new process
-        pid_t pid = fork();
+    // Create an array to store file descriptors for pipes
+    int pipe_fds[2];
 
-        // Check for fork error
-        if (pid == -1) {
-            perror("Fork failed");
-            exit(errno);  // Exit with the proper errno for fork failure
+    // Loop through the command line arguments (executable names)
+    for (int i = 1; i < argc; i++) {
+        // Check if it's not the last executable in the list
+        if (i < argc - 1) {
+            // Create a pipe for the current process
+            if (pipe(pipe_fds) == -1) {
+                perror("Pipe creation failed");
+                exit(errno);
+            }
         }
 
-if (pid == 0) {
-    // Child process
+        // Create a child process
+        pid_t child_pid = fork();
 
-    // If it's the first program, simply execute it
-    if (i == 1) {
-        if (execlp(argv[i], argv[i], (char *)NULL) == -1) {
+        if (child_pid == -1) {
+            perror("Fork failed");
+            exit(errno);
+        }
+
+        if (child_pid == 0) {
+            // Child process
+
+            // If it's not the first executable, set standard input to the read end of the pipe
+            if (i > 1) {
+                if (dup2(pipe_fds[0], STDIN_FILENO) == -1) {
+                    perror("Dup2 failed");
+                    exit(errno);
+                }
+                // Close the unused write end of the pipe
+                close(pipe_fds[1]);
+            }
+
+            // If it's not the last executable, set standard output to the write end of the pipe
+            if (i < argc - 1) {
+                if (dup2(pipe_fds[1], STDOUT_FILENO) == -1) {
+                    perror("Dup2 failed");
+                    exit(errno);
+                }
+                // Close the unused read end of the pipe
+                close(pipe_fds[0]);
+            }
+
+            // Execute the current program in the child process
+            execlp(argv[i], argv[i], NULL);
+
+            // If execlp fails, print an error message and exit
             perror("Exec failed");
             exit(errno);
-        }
-    } else {
-        int pipefd[2];
-
-        // Create a pipe
-        if (pipe(pipefd) == -1) {
-            perror("Pipe creation failed");
-            exit(errno);
-        }
-
-        // Fork a new process for the current program
-        pid_t curr_pid = fork();
-
-        if (curr_pid == -1) {
-            perror("Fork failed");
-            exit(errno);
-        }
-
-        if (curr_pid == 0) {
-            // Child process of the current program
-
-            // Close the write end of the pipe
-            close(pipefd[1]);
-
-            // Redirect standard input to the pipe
-            dup2(pipefd[0], STDIN_FILENO);
-
-            // Execute the current program
-            if (execlp(argv[i], argv[i], (char *)NULL) == -1) {
-                perror("Exec failed");
-                exit(errno);
-            }
         } else {
-            // Parent process (original child process of the first program)
+            // Parent process
 
-            // Close the read end of the pipe
-            close(pipefd[0]);
-
-            // Redirect standard output to the pipe
-            dup2(pipefd[1], STDOUT_FILENO);
-
-            // Execute the first program
-            if (execlp(argv[1], argv[1], (char *)NULL) == -1) {
-                perror("Exec failed");
-                exit(errno);
+            // If it's not the first executable, close the unused read end of the pipe
+            if (i > 1) {
+                close(pipe_fds[0]);
             }
-        }
-    }
+            // If it's not the last executable, close the unused write end of the pipe
+            if (i < argc - 1) {
+                close(pipe_fds[1]);
+            }
 
-    // This part is reached only if there was an error in the exec calls
-    perror("Exec failed");
-    exit(errno);
-}
-
-    }
-
-    // Parent process: Wait for all child processes to complete
-    for (int i = 1; i < argc; i++) {
-        int status;
-        pid_t child_pid = waitpid(-1, &status, 0);
-
-        // Check for waitpid error
-        if (child_pid == -1) {
-            perror("Waitpid failed");
-            exit(errno);  // Exit with the proper errno for waitpid failure
+            // Wait for the child process to finish
+            wait(NULL);
         }
     }
 
